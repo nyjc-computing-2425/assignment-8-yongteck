4c38b9d16143289dd94d8e831b56873047155c27
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-04-01 08:37:56 +0000

    add test_main

--- test_main.py
+++ test_main.py
@@ -1,15 +1,19 @@
 import sys
 import unittest
 
+import main
+
+
 # autograding submodule might not be successfully pulled on init
 # if unsuccessful, we have to pull it manually
 # limit number of retries
 MAX_RETRIES = 5
 retries = 0
+
 while "autograding" not in sys.modules:
     try:
         import autograding
-        from autograding.case import FuncCall, InOut
+        from autograding.case import FuncCall, InOut, RecursiveCall
     except ImportError:
         import subprocess
         subprocess.run(["git", "submodule", "update", "--init"])
@@ -18,8 +22,52 @@ while "autograding" not in sys.modules:
         sys.exit("[import autograding] Too many retries, exiting")
 
 
-# TestMyCode(autograding.TestInputOutput):
-# TestMyFunction(autograding.TestFunction):
+class TestMyRecursive(autograding.TestRecursive):
+    def setUp(self):
+        self.testcases = [
+            RecursiveCall(module=main,
+                          func=main.reverse,
+                          args=("",),
+                          ans="",
+                          output="",
+                          basecase=True),
+            RecursiveCall(module=main,
+                          func=main.reverse,
+                          args=("1",),
+                          ans="1",
+                          output="",
+                          basecase=True),
+            RecursiveCall(module=main,
+                          func=main.reverse,
+                          args=("12345",),
+                          ans="54321",
+                          output="",
+                          basecase=False),
+            RecursiveCall(module=main,
+                          func=main.is_palindrome,
+                          args=("",),
+                          ans=True,
+                          output="",
+                          basecase=True),
+            RecursiveCall(module=main,
+                          func=main.is_palindrome,
+                          args=("a",),
+                          ans=True,
+                          output="",
+                          basecase=True),
+            RecursiveCall(module=main,
+                          func=main.is_palindrome,
+                          args=("racecar",),
+                          ans=True,
+                          output="",
+                          basecase=False),
+            RecursiveCall(module=main,
+                          func=main.is_palindrome,
+                          args=("aloe vera",),
+                          ans=False,
+                          output="",
+                          basecase=False),
+    ]
 
 
 if __name__ == '__main__':
