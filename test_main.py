00145c491f6e7bc56c24630e3897f74406bce15b
Mr Surya <123524250+mrvsurya@users.noreply.github.com>
2024-04-02 19:31:00 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,23 +1,47 @@
+from importlib import reload
+import subprocess
 import sys
 import unittest
 
 import main
 
 
-# autograding submodule might not be successfully pulled on init
-# if unsuccessful, we have to pull it manually
 # limit number of retries
 MAX_RETRIES = 5
 retries = 0
 
-while "autograding" not in sys.modules:
+
+def fetch_autograding() -> None:
+    subprocess.run(["git", "submodule", "update", "--init", "--remote"])
+
+def autograding_successfully_imported() -> bool:
+    """Check if autograding module is successfully imported.
+
+    If a submodule is imported (e.g. autograding.case), autograding may be imported
+    as a namespace module.
+    Namespace modules have __file__ attribute set tot None, so we can use that to check
+    if autograding was imported as a module and not just a namespace module.
+    """
+    return (
+        "autograding" in locals()
+        and locals().get("autograding").__file__
+    )
+
+# Force refresh of autograding module from upstream
+fetch_autograding()
+
+# autograding submodule might not be successfully fetched on init
+# if unsuccessful, we have to fetch it manually
+while not autograding_successfully_imported():
     try:
         import autograding
+        reload(autograding)
         from autograding.case import FuncCall, InOut, RecursiveCall
-    except ImportError:
-        import subprocess
-        subprocess.run(["git", "submodule", "update", "--init"])
+    except (ImportError, ModuleNotFoundError):
+        fetch_autograding()
         retries += 1
+    else:
+        break
     if retries >= MAX_RETRIES:
         sys.exit("[import autograding] Too many retries, exiting")
 
